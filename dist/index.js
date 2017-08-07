0928c87bce8505ef4644f412de0a509c2a982eee
Augustin Peyrard <augustin.peyrard@gmail.com>
2017-08-07 00:27:23 -0700

    parse command line, allows configuration via options
    `Commander` is added to manage the program arguments and options.
    
    Need to add other capabilities, such as:
    - configuring mail output
    - use a config file (in YAML?!) to avoid too many options and persist
    configuration
    - filter watched files by extension
    (- only when complete option, which permit to wait the full download of
    a file before exposing it... maybe by debouncing the create/modif
    event... will not work if the download is hanging totally though.)

--- dist/index.js
+++ dist/index.js
@@ -4,27 +4,58 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Watcher_1 = require("./server/Watcher");
 const ConsoleNotifier_1 = require("./notification/console/ConsoleNotifier");
 const StaticFileServer_1 = require("./server/StaticFileServer");
-const DIR_TO_WATCH = "/tmp/private";
-const DIR_TO_EXPOSE = "/tmp/public";
-const PORT = 3000;
-const CERT = "localhost.crt";
-const KEY = "localhost.key";
+const program = require("Commander");
+const Colors = require("colors");
 const OUT = process.stdout;
-const start = new Date();
-/*
-    ---- Start express to expose files, then start a watcher, and pipe it to server
- */
-StaticFileServer_1.StaticFileServer.serve(DIR_TO_EXPOSE)
-    .onNewFile(ConsoleNotifier_1.WritableStreamNewFileNotifier.to(process.stdout))
-    .useSSL({ cert: CERT, key: KEY })
-    .start(PORT)
-    .then(server => {
-    OUT.write(`👓  watching ${DIR_TO_WATCH}\n`);
-    Watcher_1.default.watch(DIR_TO_WATCH)
-        .pipe(server);
-    OUT.write(`💻  server started on ${server.address} in ${elapsedTime()}ms\n`);
-});
-function elapsedTime() {
+program
+    .version("0.0.1")
+    .description("watch some directory and expose new files via web server")
+    .usage("<path-to-watch> [options]")
+    .option("--expose-out <path-to-expose>", "The path where the exposed file will be linked.", "/tmp")
+    .option("--port <port>", "The port for the web server.", 3000)
+    .option("--cert <cert-file-path>", "The path to the cert file (to use SSL, both cert and key must be defined)")
+    .option("--key <key-file-path>", "The path to the cert file (to use SSL, both cert and key must be defined)")
+    .option("--bindTo <hostname>", "The hostname where to bind the server (will be used for URLs generation)")
+    .action((pathToWatch, options) => {
+    const start = new Date();
+    /*
+        ---- Start express to expose files, then start a watcher, and pipe it to server
+     */
+    let staticFileServer = StaticFileServer_1.StaticFileServer.serve(options.exposeOut)
+        .onNewFile(ConsoleNotifier_1.WritableStreamNewFileNotifier.to(process.stdout));
+    const sslOptions = extractSSLOptions(options.cert, options.key);
+    if (sslOptions) {
+        staticFileServer = staticFileServer.useSSL(sslOptions);
+    }
+    if (options.bindTo) {
+        staticFileServer = staticFileServer.bindTo(options.bindTo);
+    }
+    staticFileServer
+        .start(options.port)
+        .then(server => {
+        OUT.write(`👓  watching ${pathToWatch}\n`);
+        Watcher_1.default.watch(pathToWatch)
+            .pipe(server);
+        OUT.write(`💻  server started on ${server.address} in ${elapsedTimeSince(start)}ms\n`);
+    });
+})
+    .parse(process.argv);
+function extractSSLOptions(cert, key) {
+    if (cert && key) {
+        return { cert, key };
+    }
+    if (cert && !key || key && !cert) {
+        OUT.write("[WARNING] to use ssl, both cert and key must be specified!\n");
+    }
+    return;
+}
+if (!process.argv.slice(2).length) {
+    program.outputHelp(make_red);
+}
+function make_red(txt) {
+    return Colors.red(txt); //display the help text in red on the console
+}
+function elapsedTimeSince(start) {
     return new Date().getTime() - start.getTime();
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,85 @@
 import Watcher from "./server/Watcher";
 import { WritableStreamNewFileNotifier } from "./notification/console/ConsoleNotifier";
 import { StaticFileServer } from "./server/StaticFileServer";
-
-const DIR_TO_WATCH = "/tmp/private";
-const DIR_TO_EXPOSE = "/tmp/public";
-const PORT = 3000;
-
-const CERT = "localhost.crt";
-const KEY = "localhost.key";
+import * as program from "Commander";
+import * as Colors from "colors";
 
 const OUT = process.stdout;
 
-const start = new Date();
-/*
-    ---- Start express to expose files, then start a watcher, and pipe it to server
- */
-StaticFileServer.serve(DIR_TO_EXPOSE)
-                .onNewFile(WritableStreamNewFileNotifier.to(process.stdout))
-                .useSSL({cert: CERT, key: KEY})
-                .start(PORT)
-                .then(server => {
-                    OUT.write(`👓  watching ${DIR_TO_WATCH}\n`);
-                    Watcher.watch(DIR_TO_WATCH)
-                           .pipe(server);
-                    OUT.write(
-                        `💻  server started on ${server.address} in ${elapsedTime()}ms\n`
-                    );
-                });
+program
+    .version("0.0.1")
+    .description("watch some directory and expose new files via web server")
+    .usage("<path-to-watch> [options]")
+    .option(
+        "--expose-out <path-to-expose>",
+        "The path where the exposed file will be linked.",
+        "/tmp"
+    )
+    .option(
+        "--port <port>",
+        "The port for the web server.",
+        3000
+    )
+    .option(
+        "--cert <cert-file-path>",
+        "The path to the cert file (to use SSL, both cert and key must be defined)"
+    )
+    .option(
+        "--key <key-file-path>",
+        "The path to the cert file (to use SSL, both cert and key must be defined)"
+    )
+    .option(
+        "--bindTo <hostname>",
+        "The hostname where to bind the server (will be used for URLs generation)"
+    )
+    .action((pathToWatch: string, options: any) => {
+        const start = new Date();
+        /*
+            ---- Start express to expose files, then start a watcher, and pipe it to server
+         */
+        let staticFileServer = StaticFileServer.serve(options.exposeOut)
+                                               .onNewFile(WritableStreamNewFileNotifier.to(process.stdout));
 
-function elapsedTime() {
+        const sslOptions = extractSSLOptions(options.cert, options.key);
+        if (sslOptions) {
+            staticFileServer = staticFileServer.useSSL(sslOptions);
+        }
+        if (options.bindTo) {
+            staticFileServer = staticFileServer.bindTo(options.bindTo);
+        }
+
+        staticFileServer
+            .start(options.port)
+            .then(server => {
+                OUT.write(`👓  watching ${pathToWatch}\n`);
+                Watcher.watch(pathToWatch)
+                       .pipe(server);
+                OUT.write(
+                    `💻  server started on ${server.address} in ${elapsedTimeSince(start)}ms\n`
+                );
+            });
+
+    })
+    .parse(process.argv);
+
+function extractSSLOptions(cert: string | undefined, key: string | undefined): StaticFileServer.SSLConfig | void {
+    if (cert && key) {
+        return { cert, key }
+    }
+    if (cert && !key || key && !cert) {
+        OUT.write("[WARNING] to use ssl, both cert and key must be specified!\n");
+    }
+    return;
+}
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp(make_red as any);
+}
+
+function make_red(txt: string) {
+    return Colors.red(txt); //display the help text in red on the console
+}
+
+function elapsedTimeSince(start: Date) {
     return new Date().getTime() - start.getTime();
 }
