e3b8e6d3ce8509a79f55835849420979edbcdd34
Augustin Peyrard <augustin.peyrard@gmail.com>
2017-08-08 00:20:44 -0700

    allows configuration files
    In order to avoid to many options, a config option is added, this option
    permits to specify a file containing the available options in a
    `key=value` properties format.
    
    Unfortunately by specifying the `fileToWatch` argument as optional, the
    `action` does not match when it is not specified, so its way ugly, but
    just use the statefull `program` object.

--- dist/index.js
+++ dist/index.js
@@ -9,48 +9,79 @@ const Colors = require("colors");
 const Mailer_1 = require("./notification/mail/Mailer");
 const Notification_1 = require("./notification/Notification");
 const MailNotifier_1 = require("./notification/mail/MailNotifier");
+const Fs = require("fs");
+const Error_1 = require("./util/Error");
 const OUT = process.stdout;
 program
     .version("0.0.1")
     .description("watch some directory and expose new files via web server")
-    .usage("<path-to-watch> [options]")
+    .usage("[path-to-watch] [options]")
     .option("--expose-out <path-to-expose>", "The path where the exposed file will be linked.", "/tmp")
-    .option("--port <port>", "The port for the web server.", 3000)
+    .option("--port <port>", "The port for the web server.")
     .option("--cert <cert-file-path>", "The path to the cert file (to use SSL, both cert and key must be defined)")
     .option("--key <key-file-path>", "The path to the cert file (to use SSL, both cert and key must be defined)")
     .option("--bindTo <hostname>", "The hostname where to bind the server (will be used for URLs generation)")
     .option("--smtp <username:password>", "The smtp server username and password (only Gmail smtp is supported)")
     .option("--email <email>", "The email to notify when a new file is exposed, this option required a smtp configuration")
-    .action((pathToWatch, options) => {
-    const start = new Date();
-    /*
-        ---- Start express to expose files, then start a watcher, and pipe it to server
-     */
-    let staticFileServer = StaticFileServer_1.StaticFileServer.serve(options.exposeOut);
-    const sslOptions = extractSSLOptions(options.cert, options.key);
-    if (sslOptions) {
-        staticFileServer = staticFileServer.useSSL(sslOptions);
-    }
-    if (options.bindTo) {
-        staticFileServer = staticFileServer.bindTo(options.bindTo);
-    }
-    let notifier = ConsoleNotifier_1.WritableStreamNewFileNotifier.to(process.stdout);
-    const mailOptions = extractMailOptions(options.smtp, options.email);
-    if (mailOptions) {
-        const [to, mailConfig] = mailOptions;
-        notifier = Notification_1.default.compose(notifier, MailNotifier_1.MailNewFileNotifier.to(to, Mailer_1.default.withConfig(mailConfig)));
-    }
-    staticFileServer = staticFileServer.onNewFile(notifier);
-    staticFileServer
-        .start(options.port)
-        .then(server => {
-        OUT.write(`👓  watching ${pathToWatch}\n`);
-        Watcher_1.default.watch(pathToWatch)
-            .pipe(server);
-        OUT.write(`💻  server started on ${server.address} in ${elapsedTimeSince(start)}ms\n`);
-    });
-})
+    .option("--config <config-file-path>", "The config file to read options from.")
     .parse(process.argv);
+/*
+    --- main
+ */
+const start = new Date();
+const computedOptions = extractOptions(program);
+const pathToWatch = program.pathToWatch || computedOptions.watch;
+if (!pathToWatch) {
+    program.outputHelp(make_red);
+    process.exit(-1);
+}
+/*
+    ---- Start express to expose files, then start a watcher, and pipe it to server
+ */
+let staticFileServer = StaticFileServer_1.StaticFileServer.serve(computedOptions.exposeOut);
+const sslOptions = extractSSLOptions(computedOptions.cert, computedOptions.key);
+if (sslOptions) {
+    staticFileServer = staticFileServer.useSSL(sslOptions);
+}
+if (computedOptions.bindTo) {
+    staticFileServer = staticFileServer.bindTo(computedOptions.bindTo);
+}
+let notifier = ConsoleNotifier_1.WritableStreamNewFileNotifier.to(process.stdout);
+const mailOptions = extractMailOptions(computedOptions.smtp, computedOptions.email);
+if (mailOptions) {
+    const [to, mailConfig] = mailOptions;
+    notifier = Notification_1.default.compose(notifier, MailNotifier_1.MailNewFileNotifier.to(to, Mailer_1.default.withConfig(mailConfig)));
+}
+staticFileServer = staticFileServer.onNewFile(notifier);
+staticFileServer
+    .start(computedOptions.port || 3000)
+    .then(server => {
+    OUT.write(`👓  watching ${pathToWatch}\n`);
+    Watcher_1.default.watch(pathToWatch)
+        .pipe(server);
+    OUT.write(`💻  server started on ${server.address} in ${elapsedTimeSince(start)}ms\n`);
+});
+function extractOptions(clOptions) {
+    if (clOptions.config) {
+        try {
+            return Object.assign({}, Fs.readFileSync(clOptions.config)
+                .toString()
+                .split("\n")
+                .reduce((acc, cur) => {
+                const [key, value] = cur.split("=");
+                if (key && value) {
+                    acc[key] = value;
+                }
+                return acc;
+            }, {}), clOptions);
+        }
+        catch (ex) {
+            throw new Error(`unable to extract options ${clOptions.config}!
+${Error_1.isNoFileFound(ex) && "-- unable to find config file!"}`);
+        }
+    }
+    return clOptions;
+}
 function extractSSLOptions(cert, key) {
     if (cert && key) {
         return { cert, key };
@@ -77,9 +108,6 @@ function extractMailOptions(smtp, to) {
         OUT.write("[WARNING] to enable mail notifications, both email and smtp options must be specified!\n");
     }
     return;
-}
-if (!process.argv.slice(2).length) {
-    program.outputHelp(make_red);
 }
 function make_red(txt) {
     return Colors.red(txt); //display the help text in red on the console
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,15 @@ import * as Colors from "colors";
 import Mailer from "./notification/mail/Mailer";
 import Notification from "./notification/Notification";
 import { MailNewFileNotifier } from "./notification/mail/MailNotifier";
+import * as Fs from "fs";
+import { isNoFileFound } from "./util/Error";
 
 const OUT = process.stdout;
 
 program
     .version("0.0.1")
     .description("watch some directory and expose new files via web server")
-    .usage("<path-to-watch> [options]")
+    .usage("[path-to-watch] [options]")
     .option(
         "--expose-out <path-to-expose>",
         "The path where the exposed file will be linked.",
@@ -23,7 +25,6 @@ program
     .option(
         "--port <port>",
         "The port for the web server.",
-        3000
     )
     .option(
         "--cert <cert-file-path>",
@@ -45,49 +46,94 @@ program
         "--email <email>",
         "The email to notify when a new file is exposed, this option required a smtp configuration"
     )
-    .action((pathToWatch: string, options: any) => {
-        const start = new Date();
-        /*
-            ---- Start express to expose files, then start a watcher, and pipe it to server
-         */
-        let staticFileServer = StaticFileServer.serve(options.exposeOut);
-
-        const sslOptions = extractSSLOptions(options.cert, options.key);
-        if (sslOptions) {
-            staticFileServer = staticFileServer.useSSL(sslOptions);
-        }
-        if (options.bindTo) {
-            staticFileServer = staticFileServer.bindTo(options.bindTo);
-        }
-
-        let notifier: Notification.Notifier<Notification.NewFileEvent> =
-            WritableStreamNewFileNotifier.to(process.stdout);
-        const mailOptions = extractMailOptions(options.smtp, options.email);
-        if (mailOptions) {
-            const [to, mailConfig] = mailOptions;
-            notifier = Notification.compose(
-                notifier,
-                MailNewFileNotifier.to(
-                    to,
-                    Mailer.withConfig(mailConfig)
-                )
-            )
-        }
-        staticFileServer = staticFileServer.onNewFile(notifier);
-
-        staticFileServer
-            .start(options.port)
-            .then(server => {
-                OUT.write(`👓  watching ${pathToWatch}\n`);
-                Watcher.watch(pathToWatch)
-                       .pipe(server);
-                OUT.write(
-                    `💻  server started on ${server.address} in ${elapsedTimeSince(start)}ms\n`
-                );
-            });
-
-    })
+    .option(
+        "--config <config-file-path>",
+        "The config file to read options from."
+    )
     .parse(process.argv);
+
+/*
+    --- main
+ */
+
+const start = new Date();
+
+const computedOptions = extractOptions(program);
+const pathToWatch = program.pathToWatch || computedOptions.watch;
+
+if (!pathToWatch) {
+    program.outputHelp(make_red as any);
+    process.exit(-1);
+}
+
+/*
+    ---- Start express to expose files, then start a watcher, and pipe it to server
+ */
+let staticFileServer = StaticFileServer.serve(computedOptions.exposeOut);
+
+const sslOptions = extractSSLOptions(computedOptions.cert, computedOptions.key);
+if (sslOptions) {
+    staticFileServer = staticFileServer.useSSL(sslOptions);
+}
+if (computedOptions.bindTo) {
+    staticFileServer = staticFileServer.bindTo(computedOptions.bindTo);
+}
+
+let notifier: Notification.Notifier<Notification.NewFileEvent> =
+    WritableStreamNewFileNotifier.to(process.stdout);
+const mailOptions = extractMailOptions(computedOptions.smtp, computedOptions.email);
+if (mailOptions) {
+    const [to, mailConfig] = mailOptions;
+    notifier = Notification.compose(
+        notifier,
+        MailNewFileNotifier.to(
+            to,
+            Mailer.withConfig(mailConfig)
+        )
+    )
+}
+staticFileServer = staticFileServer.onNewFile(notifier);
+
+staticFileServer
+    .start(computedOptions.port || 3000)
+    .then(server => {
+        OUT.write(`👓  watching ${pathToWatch}\n`);
+        Watcher.watch(pathToWatch)
+               .pipe(server);
+        OUT.write(
+            `💻  server started on ${server.address} in ${elapsedTimeSince(start)}ms\n`
+        );
+    });
+
+function extractOptions(clOptions: any): any {
+    if (clOptions.config) {
+        try {
+            return {
+                ...Fs.readFileSync(clOptions.config)
+                     .toString()
+                     .split("\n")
+                     .reduce(
+                         (acc: any, cur: string) => {
+                             const [key, value] = cur.split("=");
+                             if (key && value) {
+                                 acc[key] = value;
+                             }
+                             return acc;
+                         },
+                         {}
+                     ),
+                ...clOptions
+            }
+        } catch (ex) {
+            throw new Error(
+                `unable to extract options ${clOptions.config}!
+${isNoFileFound(ex) && "-- unable to find config file!"}`
+            );
+        }
+    }
+    return clOptions;
+}
+
 
 function extractSSLOptions(cert: string | undefined, key: string | undefined): StaticFileServer.SSLConfig | void {
     if (cert && key) {
@@ -116,10 +162,6 @@ function extractMailOptions(smtp: string, to: string): [string, Mailer.Config] |
         OUT.write("[WARNING] to enable mail notifications, both email and smtp options must be specified!\n");
     }
     return;
-}
-
-if (!process.argv.slice(2).length) {
-    program.outputHelp(make_red as any);
 }
 
 function make_red(txt: string) {
